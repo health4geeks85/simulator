c28ff31ea2b6e4331eeb239388b71c1db958d788
unknown <health4geeks85@gmail.com>
2018-03-07 11:51:16 +0700

    Update lib name

--- hyperledger.py
+++ hyperledger.py
@@ -13,33 +13,41 @@ dateFormat = '%Y-%m-%d'
 timeFormat = '%H:%M:%S'
 
 OPENING_TEXT = '''
-*****************************************************************************************
-*      COPYRIGHT (c) 2017 GIA TRAN COMPANY                                              *
-*      All rights reserved.                                                             *
-*      BLOCKCHAIN PROJECT                                                               * 
-*                                                                                       * 
-*  * Redistributions of source code must retain the above copyright notice, this        *
-*    list of conditions and the following disclaimer.                                   *
-*                                                                                       *
-*  * Redistributions in binary form must reproduce the above copyright notice,          *
-*    this list of conditions and the following disclaimer in the documentation          *
-*    and/or other materials provided with the distribution.                             *
-*                                                                                       *
-*  * Neither the name of the copyright holders, nor those of its contributors           *
-*    may be used to endorse or promote products derived from this software without      *
-*    specific prior written permission.                                                 *
-*                                                                                       *
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND      *
-*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED        *
-*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE               *
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE         *   
-*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL           *   
-*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR           *
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER           *
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,        *
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE        *
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                 *
-*****************************************************************************************
+*****************************************************************************************************************
+*   _____ _____            _______ _____            _   _    _____ ____  __  __ _____        _   ___     _ _    *
+*  / ____|_   _|   /\     |__   __|  __ \     /\   | \ | |  / ____/ __ \|  \/  |  __ \ /\   | \ | \ \   / /     *
+* | |  __  | |    /  \       | |  | |__) |   /  \  |  \| | | |   | |  | | \  / | |__) /  \  |  \| |\ \_/ /      * 
+* | | |_ | | |   / /\ \      | |  |  _  /   / /\ \ | . ` | | |   | |  | | |\/| |  ___/ /\ \ | . ` | \   /       * 
+* | |__| |_| |_ / ____ \     | |  | | \ \  / ____ \| |\  | | |___| |__| | |  | | |  / ____ \| |\  |  | |        * 
+*  \_____|_____/_/    \_\    |_|  |_|  \_\/_/    \_\_| \_|  \_____\____/|_|  |_|_| /_/    \_\_| \_|  |_|        *
+*                                                                                                               * 
+*                                                                                                               *
+*      COPYRIGHT (c) 2017 GIA TRAN COMPANY                                                                      *
+*      All rights reserved.                                                                                     *
+*      BLOCKCHAIN PROJECT                                                                                       *                         
+*                                                                                                               *                         
+*  * Redistributions of source code must retain the above copyright notice, this                                *                        
+*    list of conditions and the following disclaimer.                                                           *
+*                                                                                                               *
+*  * Redistributions in binary form must reproduce the above copyright notice,                                  *
+*    this list of conditions and the following disclaimer in the documentation                                  *
+*    and/or other materials provided with the distribution.                                                     *
+*                                                                                                               *
+*  * Neither the name of the copyright holders, nor those of its contributors                                   *
+*    may be used to endorse or promote products derived from this software without                              *
+*    specific prior written permission.                                                                         *
+*                                                                                                               *
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND                              *
+*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED                                *
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE                                       *
+*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE                                 *                           
+*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL                                   *   
+*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR                                   *
+*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER                                   *
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,                                *
+*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE                                *
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                         *
+*****************************************************************************************************************
 '''
 EXIT_BOOL = False
 
@@ -71,6 +79,8 @@ def display(path):
     print(Fore.RED)
     print(OPENING_TEXT)
     print(Style.RESET_ALL)
+    print("Initializing.....")
+    time.sleep(2)
 
     # register exit signals
     registerSignals()
